4b27a3a39273f4a41348f8a242944de96f07370d
CodeArmer <geanmotas@gmail.com>
2023-12-01 06:19:38 -0400

    Refactor form handling and add conditional rendering

--- app.py
+++ app.py
@@ -12,13 +12,20 @@ def ejercicio1():
     return render_template('ejercicio1.html')
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    print(request.form)
+    return render_template('ejercicio1.html', 
+    name =  request.form['name'],
+    age = int(request.form['age']),
+    paint = int(request.form['paint']),
+    )
 @app.route('/ejercicio2')
 def ejercicio2():
     return render_template('ejercicio2.html')
 @app.route('/login', methods=['POST'])
 def login():
-    print(request.form)
+    return render_template('ejercicio2.html', 
+    username =  request.form['username'],
+    password = request.form['password'],
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
