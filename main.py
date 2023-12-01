9cca79bb05b1a1185796c7429a975fd08938a359
CodeArmer <geanmotas@gmail.com>
2023-12-01 03:01:59 -0400

    Created templates

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/ejercicio1')
+def ejercicio1():
+    return render_template('ejercicio1.html')
+@app.route('/ejercicio2')
+def ejercicio2():
+    return render_template('ejercicio2.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
