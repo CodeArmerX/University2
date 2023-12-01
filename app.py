363fffe9a0f3a621b2a0aaae1f3e019fec39579d
CodeArmer <geanmotas@gmail.com>
2023-12-01 04:56:43 -0400

    Delete unnecessary routes and update form inputs in templates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,9 +10,15 @@ def index():
 @app.route('/ejercicio1')
 def ejercicio1():
     return render_template('ejercicio1.html')
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    print(request.form)
 @app.route('/ejercicio2')
 def ejercicio2():
     return render_template('ejercicio2.html')
+@app.route('/login', methods=['POST'])
+def login():
+    print(request.form)
 
 if __name__ == '__main__':
     app.run(debug=True)
